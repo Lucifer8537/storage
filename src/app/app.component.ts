9a8fc99aa7e47fa7dac5de8109980140d06cb695
navodit <navodit50@gmail.com>
2023-11-27 23:16:35 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Renderer2,
+  OnInit,
+  HostListener,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'storage';
+  rangeValue: number = 815;
+  totalSize!: number;
+  left!: string;
+  right!: string;
+  leftMiddle!: string;
+  mobileView!: boolean;
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+  ngOnInit(): void {
+    this.onResize();
+    this.totalSize = 1000 - this.rangeValue;
+    this.left = this.rangeValue / 10 + '%';
+    this.right = this.totalSize / 10 + '%';
+    this.leftMiddle = this.rangeValue / 20 + '%';
+    console.log('this.left : ', this.left);
+    console.log('this.right : ', this.right);
+    console.log('this.leftMiddle : ', this.leftMiddle);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    console.log(window.innerWidth);
+    if (window.innerWidth < 678) {
+      this.mobileView = true;
+    } else {
+      this.mobileView = false;
+    }
+  }
+
+  onSliderChange = () => {
+    console.log('It is called : ', this.rangeValue);
+    const slider = this.el.nativeElement.querySelector('input.slider');
+    this.totalSize = 1000 - this.rangeValue;
+    this.left = this.rangeValue / 10 + '%';
+    this.right = this.totalSize / 10 + '%';
+    this.leftMiddle = this.rangeValue / 20 + '%';
+    console.log('this.left : ', this.left);
+    console.log('this.right : ', this.right);
+    console.log('this.leftMiddle : ', this.leftMiddle);
+  };
 }
